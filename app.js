239be348727d168563d13f3c091bc7821fc9cdf2
Gasamagera <iradukundaprince561@gmail.com>
2024-11-24 11:35:14 +0000

    Designing Model for Posts

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const userRouter = require("./routes/userRoutes");
+const postRouter = require("./routes/postRoutes");
 
 const app = express();
 
 app.use(express.json({ limit: "10kb" }));
 
 app.use("/api/v1/users", userRouter);
+app.use("/api/v1/posts", postRouter);
 
 module.exports = app;
